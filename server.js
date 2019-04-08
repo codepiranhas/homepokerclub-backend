6e49372166ca24f30fdc81fac04a9a841478e570
Georgios Fandridis <fandridis@gmail.com>
2019-04-08 23:27:34 +0200

    remove useless comments

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 ﻿const app = require('./app.js');
 
-const port = process.env.PORT || 4000; //  process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const port = process.env.PORT || 4000;
 
 const server = app.listen(port, function () {
     console.log('Server listening on port ' + port);
