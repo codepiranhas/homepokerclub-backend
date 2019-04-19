3619f2d645c16c13d967b4e091a1ae9600a54bfd
Georgios Fandridis <fandridis@gmail.com>
2019-04-19 13:30:42 +0200

    Only require local config locally

--- config/keys.js
+++ config/keys.js
@@ -2,12 +2,12 @@ console.log('===> NODE_ENV @ KEYS: ', process.env.NODE_ENV); // eslint-disable-l
 
 const productionKeys = require('./production');
 const developmentKeys = require('./development');
-const localKeys = require('./local');
 
 if (process.env.NODE_ENV === 'production') {
 	module.exports = productionKeys;
 } else if (process.env.NODE_ENV === 'development') {
 	module.exports = developmentKeys;
 } else {
+	const localKeys = require('./local'); // eslint-disable-line
 	module.exports = localKeys;
 }
