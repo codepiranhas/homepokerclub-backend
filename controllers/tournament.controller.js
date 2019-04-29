2f18b9715306a45c9fb092eb947f7e74abfa6b68
George Fandridis <fandridis@gmail.com>
2019-04-29 09:17:14 +0200

    Refactor routes and controllers to pass the req, res, next objects (#20)

--- controllers/tournament.controller.js
+++ controllers/tournament.controller.js
@@ -8,18 +8,28 @@ const db = require('../helpers/db');
 // const tokenService = require('services/token.service');
 const TournamentModel = db.Tournament;
 
-async function create(tournamentParam) {
+async function create(req, res, next) {
+	const tournamentParam = req.body;
+
 	if (!tournamentParam || !tournamentParam.name || !tournamentParam.clubId) {
-		throw new Error('Invalid parameters');
+		return next(Error('Invalid parameters'));
 	}
 
 	const savedTournament = await TournamentModel.create(tournamentParam);
 
-	return { tournament: savedTournament };
+	return res.status(200).json({ tournament: savedTournament });
 }
 
-async function deleteTournament(tournamentId) {
+async function deleteTournament(req, res, next) {
+	const tournamentId = req.params.id;
+
+	if (!tournamentId) {
+		return next(Error('Invalid parameters'));
+	}
+
 	await TournamentModel.deleteById(tournamentId);
+
+	return res.status(200).json({ message: 'success' });
 }
 
 module.exports = {
--- routes/club.routes.js
+++ routes/club.routes.js
@@ -3,43 +3,11 @@ const clubController = require('../controllers/club.controller');
 
 const router = express.Router();
 
-function create(req, res, next) {
-	clubController.create(req.body, { currentUserId: req.user.sub })
-		.then(data => res.json(data))
-		.catch(err => next(err));
-}
-
-function addMember(req, res, next) {
-	clubController.addMember(req.body, { currentUserId: req.user.sub, currentClubId: req.params.id })
-		.then(data => res.json(data))
-		.catch(err => next(err));
-}
-
-function deleteClub(req, res, next) {
-	clubController.deleteClub(req.params.id)
-		.then(data => res.json(data))
-		.catch(err => next(err));
-}
-
-function acceptInvitation(req, res, next) {
-	clubController.acceptInvitation({ currentUserId: req.user.sub, currentClubId: req.params.id })
-		.then(data => res.json(data))
-		.catch(err => next(err));
-}
-
-function declineInvitation(req, res, next) {
-	clubController.declineInvitation({ currentUserId: req.user.sub, currentClubId: req.params.id })
-		.then(data => res.json(data))
-		.catch(err => next(err));
-}
-
 // routes
-router.post('/create', create);
-
-router.delete('/:id/delete', deleteClub);
-router.post('/:id/addMember', addMember);
-
-router.patch('/:id/acceptInvitation', acceptInvitation);
-router.patch('/:id/declineInvitation', declineInvitation);
+router.post('/create', clubController.create);
+router.delete('/:id/delete', clubController.deleteClub);
+router.post('/:id/addMember', clubController.addMember);
+router.patch('/:id/acceptInvitation', clubController.acceptInvitation);
+// router.patch('/:id/declineInvitation', clubController.declineInvitation);
 
 module.exports = router;
