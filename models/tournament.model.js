6bf048254fe2fde3983c0b631bd333c9f3340657
Georgios Fandridis <fandridis@gmail.com>
2019-06-06 10:14:39 +0200

    Update schema

--- models/tournament.model.js
+++ models/tournament.model.js
@@ -5,11 +5,28 @@ const {	Schema } = mongoose;
 const schema = new Schema({
 	name: { type: String, required: true },
 
+	buyIn: { type: Number, required: true },
+	maxPlayers: { type: Number, required: true },
+	startingChips: { type: Number, required: true },
+	levelDuration: { type: Number, required: true },	// TODO: Make sure the correct type is chosen
+
+	blinds: {
+		small: { type: Number, required: true },
+		big: { type: Number, required: true },
+	},
+
+	payoutOptions: {
+		positions: { type: Number, require: true },
+		distribution: { type: String, require: true },	// Between: ['default', 'evenly']
+	},
+
 	clubId: { type: Schema.Types.ObjectId, ref: 'Club' },
 
 	players: [{ type: Schema.Types.ObjectId, ref: 'User' }],
 
 	createdAt: { type: Date, default: Date.now },
+	startsAt: { type: Date },
+	finishedAt: { type: Date },
 });
 
 schema.set('toJSON', { virtuals: true });
