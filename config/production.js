150f100942d756b6710e94e4ea77af137ccee81c
Georgios Fandridis <fandridis@gmail.com>
2019-06-14 11:30:21 +0200

    Update env variables for upload files to S3

--- config/production.js
+++ config/production.js
@@ -3,6 +3,9 @@ module.exports = {
 	sendgridApiKey: process.env.SENDGRID_KEY,
 	mongoURI: process.env.MONGO_URI,
 
+	awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
+	awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+
 	s3BucketName: 'homepokerclub',
 
 	frontendUrl: 'http://homepokerclub.netlify.com',
